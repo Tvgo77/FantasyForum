85b0d253201bac6978c3c85a3eff23d752c5a9bd
Tvgo77 <tvgoshao77@gmail.com>
2024-05-18 11:43:17 +0000

    add TestMode bool in env

--- go-backend/app/main.go
+++ go-backend/app/main.go
@@ -17,7 +17,7 @@ func main() {
 
 	/* Connect to database */
 	passwd := env.DBpassword
-	dsn := "host=localhost user=postgres dbname=forumdb port=5432 sslmode=disable password=" + passwd
+	dsn := "host=localhost user=postgres dbname=forumdb_test port=5432 sslmode=disable password=" + passwd
 	db, err := database.NewDatabase(dsn)
 	if err != nil {
 		log.Fatal(err)
@@ -28,12 +28,22 @@ func main() {
 	if env.RunMigration {
 		db.AutoMigrate(&domain.User{})
 	}
+
+	if env.TestMode {
+		tx := db.Begin()
+		defer tx.Rollback()
+		db = database.NewDatabaseFromExist(tx)
+	}
 	
 	/* Setup router */
 	ginEngine := gin.Default()
 	publicRouter := ginEngine.Group("")
+	privateRouter := ginEngine.Group("")
 
 	router.SignupRouterSetup(env, db, publicRouter)
+	router.LoginRouterSetup(env, db, publicRouter)
+	router.ProfileRouterSetup(env, db, privateRouter)
 
 	/* Run */
+	ginEngine.Run(":8080")
 }
